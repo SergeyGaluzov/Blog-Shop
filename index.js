a9a84f9224acb442f291438fce1b44db54d27b92
Vlad Bushinskiy <42681515+vbshnsk@users.noreply.github.com>
2019-11-17 20:23:19 +0200

    routing factoring

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require("express")
-const app = express();
+const app = express()
 const path = require('path')
 
-app.use(express.static(__dirname));
+const indexRouter = require('./routes/index')
+
+app.use(express.static(__dirname))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set("view engine", "pug");
 
-app.get("/", function(request, response){
-  response.render("main", {title: "Главная Страница"});
-}); 
-
-
-app.get("/test", function(request, response){
-  response.sendFile(__dirname + "/html/index.html");
-}); 
+app.use('/', indexRouter)
 
 app.listen(3000)

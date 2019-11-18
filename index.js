1f7d0c2a0cb474fa0d8ac2991e5ce2f03ff0b80f
Sergey Galuzov <seregasuper2000@gmail.com>
2019-11-18 10:50:33 +0200

    Add form for new post

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(__dirname));
+app.set("view engine", "pug");
 app.get("/", function(request, response){
   response.sendFile(__dirname + "/html/index.html");
 });
-app.listen(3000);
+app.get("/new_post", function(request, response){
+  response.render('post_template.pug');
+});
+app.post("/new_post", urlencodedParser, function(request, response) {
+  response.redirect(307, "/posts");
+});
+app.post("/posts", urlencodedParser, function(request, response) {
+  text = request.body.post_text
+  console.log(text);
+});

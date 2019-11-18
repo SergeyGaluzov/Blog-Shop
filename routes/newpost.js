6ce52d27db9013157f2bb184d1df4273e3e84feb
Vlad Bushinskiy <42681515+vbshnsk@users.noreply.github.com>
2019-11-18 23:40:25 +0200

    File upload basics

--- routes/newpost.js
+++ routes/newpost.js
@@ -1,16 +1,29 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
-const Post = require('../models/post');
-const User = require('../models/user');
+const express = require('express')
+const router = express.Router()
+const Post = require('../models/post')
+const User = require('../models/user')
 
+const multer  = require('multer')
+const storage = multer.diskStorage({
+  destination: (req, file, callback) =>{
+    callback(null, "img/uploaded/")
+  },
+  filename: (req, file, callback) =>{
+    const splittedType = file.mimetype.split("/")
+    const extension = splittedType[splittedType.length - 1]
+    callback(null, Date.now() + '.' + extension)
+  }
+})
+const upload = multer({ storage: storage })
 
 router.get('/', function(request, response) { 
   response.render("blog/newpost", {isLoggedIn: request.session.userId ? true : false});
 });
 
-router.post('/', function(request, response){
+router.post('/', upload.single('postImage'), function(request, response){
+  console.log(request.file)
   User.findById(request.session.userId, (err, user) =>{
     const postInfo = {
       userID: user._id,
@@ -18,6 +31,7 @@ router.post('/', function(request, response){
       text: request.body.text,
       title: request.body.title,
       date: new Date(),
+      imagePath: request.file.path,
     };
     Post.create(postInfo, (err, post) => {
       response.redirect("/posts");

f42e49d7e40485471bb3d038eb74b8af2669a35f
Vlad Bushinskiy <42681515+vbshnsk@users.noreply.github.com>
2019-11-21 14:21:25 +0200

    some problem here...

--- routes/posts.js
+++ routes/posts.js
@@ -3,37 +3,47 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('../models/post')
-const User = require('../models/user')
 const utils = require('../util/utils')
+const fs = require('fs')
 
+const getPostData = post => {
+    return {
+        username: utils.capitalize(post.username),
+        date: utils.dateHandler(post.date),
+        text: post.text,
+        title: utils.capitalize(post.title),
+        imagePath: post.imagePath ? "../" + post.imagePath : undefined,
+        postId: post._id,
+        userId: post.userID,
+    }
+}
 
 router.get('/', (req, res) => {
     Post.find({}, (err, posts) => {
-        const postsData = posts.map(post => {
-            return {
-                username: utils.capitalize(post.username),
-                date: utils.dateHandler(post.date),
-                text: post.text,
-                title: utils.capitalize(post.title),
-                imagePath: post.imagePath ? "../" + post.imagePath : undefined,
-                postId: post._id,
-            }
-        })
+        const postsData = posts.map(post => getPostData(post))
         res.render('blog/posts', { "posts": postsData, })
-        })
     })
+})
 
 router.get('/:postId', (req, res) => {
     Post.findById(req.params.postId, (error, post) =>{
-        const postData = {
-            username: utils.capitalize(post.username),
-                date: utils.dateHandler(post.date),
-                text: post.text,
-                title: utils.capitalize(post.title),
-                imagePath: post.imagePath ? "../" + post.imagePath : undefined,
-        }
-        res.render("blog/post", {post: postData});
+        const postData = getPostData(post)
+        res.render('blog/post', {post: postData, userId: req.session.userId});
     })
+})
+
+router.post('/:postId', (req, res) =>{
+    if(req.body.delete){
+        Post.findByIdAndDelete(req.params.postId, (err, post) =>{
+            const imagePath = getPostData(post).imagePath
+            if(imagePath){
+                fs.unlink(imagePath, (err) => {
+                    res.redirect('/posts');
+                })
+            }
+            res.redirect('/posts');
+        })
+    }
 })
 
 module.exports = router
